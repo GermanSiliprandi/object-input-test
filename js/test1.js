ce669766a3a3046d07a30caff1507eca86ecfe2f
German Siliprandi <german.siliprandi@gmail.com>
2021-11-08 19:15:55 -0300

    Object Input Test

---
+++ js/test1.js
@@ -0,0 +1,30 @@
+class product{
+    constructor(name,amount,price){
+        this.name=name.toUpperCase();
+        this.amount=parseInt(amount);
+        this.price=parseFloat(price);
+    }
+    totalPrice(){
+        return this.amount*this.price;
+    }
+}
+function dataEntry(){
+    let exit1=true;
+    while (exit1==true) {
+        quit=prompt("Please enter your products in the next popup. If you want to exit, enter Q").toUpperCase();
+        if (quit=="Q" || quit==null) {
+            exit1=false;
+        } else {
+            products.push(new product(prompt("Product Name: "), prompt("Product Amount: "), prompt("Product Price: ")));
+        }
+    }
+    }
+const products =[];
+var sum=0;
+dataEntry();
+for (i in products){
+    sum += products[i].totalPrice();
+} 
+console.log(products);
+alert("The total value of all your products is: "+ sum);
+alert("Thank you for using our services. Have a nice day");
